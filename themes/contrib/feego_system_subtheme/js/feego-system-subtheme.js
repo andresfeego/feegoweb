2324916f6a52a33a8636fd7c10d7ae40c5c8d3f8
Andres Manrique <andres.feego@gmail.com>
2024-09-28 03:25:38 -0500

    Update error tema feego tema

---
+++ themes/contrib/feego_system_subtheme/js/feego-system-subtheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * feego_system_subtheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.feegoSystemSubtheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
