b3a9248109859b13d531ca1d3a5378a87794c587
Andres Manrique <andres.feego@gmail.com>
2025-01-20 17:48:03 -0500

    • Update > Servicios - Solicitudes • Fix > Titulos

--- themes/custom/feego_sys/js/0_feego_sys.js
+++ themes/custom/feego_sys/js/0_feego_sys.js
@@ -12,10 +12,15 @@
       jQuery('.path-solicitudes .views-row .views-field-field-request-state .field-content').each(function (){
         if (!jQuery(this).hasClass( "bar" )) {
           jQuery(this).addClass('state-request-' + jQuery(this)[0].innerHTML.toLowerCase().replace(' ','-') );
-          console.log(jQuery(this))
         }
       })
+      
+      jQuery('.path-servicios .views-row .views-field-field-service-state .field-content').each(function (){
+        if (!jQuery(this).hasClass( "bar" )) {
+          jQuery(this).addClass('state-service-' + jQuery(this)[0].innerHTML.toLowerCase().replace(' ','-') );
+        }
+      })
+      
     }
   };
-
 })(Drupal);
